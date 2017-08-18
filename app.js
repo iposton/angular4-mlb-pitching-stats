ff9d0b42ac8b120b1cf5dc8c5c49576bcf048b7f
iposton <ianposton@sbcglobal.net>
2017-08-17 17:07:14 -0700

    heroku

--- app.js
+++ app.js
@@ -12,15 +12,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
 
-app.get('/main.bundle.js', function(req, res){
-       
-       res.write("let FIREBASE_URL='"+process.env.FIREBASE_URL+"'" + '\n');
-       res.write("let API_KEY='"+process.env.API_KEY+"'" + '\n');
-       res.write("let AUTH_DOM='"+process.env.AUTH_DOM+"'" + '\n');
-       res.write("let TOKEN='"+process.env.PP_API_KEY+"'" + '\n');
-       res.end();
-});
-
 const port = process.env.PORT || '3001';
 app.set('port', port);
 
